c4cac9c1f52c6d9452a24bbd7b22e277857b701b
Geert Huls <geert.huls82@gmail.com>
2019-05-06 22:21:08 +0200

    Load async api call server side

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,52 @@
 import React from 'react';
-import '../src/DigitalClock'
-import DigitalClock from '../src/DigitalClock';
+import axios from 'axios';
 
 class Index extends React.Component {
 
 
     static async getInitialProps() {
-        const promise = new Promise((resolve, reject) =>
-            setInterval(() => resolve({
-                time: new Date().toISOString()
-            }), 3000)
-        );
-
+        var promise = axios.get('http://localhost:4000/speakers')
+            .then(response => {
+                return {
+                    hasErrored: false,
+                    speakerData: response.data
+                };
+            })
+            .catch(error => {
+                return {
+                    hasErrored: true,
+                    message: error.message
+                }
+            });
         return promise;
     }
 
     constructor(props) {
         super(props);
         this.state = {
-            time: props.time
+            hasErrored: props.hasErrored,
+            message: props.message,
+            speakerData: props.speakerData
         }
     }
 
-    tick() {
-        this.setState(() => {
-            return ({
-                time: new Date().toLocaleString()
-            });
-        });
-    }
-
     componentDidMount() {
-        this.interval = setInterval(() => this.tick(), 1000);
     }
 
     componentWillUnmount() {
-        clearInterval(this.interval);
     }
 
 
     render() {
-        return <DigitalClock time={this.state.time} />;
+        return (
+            <ul>
+                {this.state.speakerData.map((speaker) =>
+                    <li key={speaker.id}>
+                        {speaker.firstName} {speaker.lastName}
+                    </li>
+                )}
+            </ul>
+        )
     }
 }
 
